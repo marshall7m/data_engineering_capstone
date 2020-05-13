16dd7aabe87a04e3fc4edfa8708a9c640549b828
marshall7m <marshallmamiya@gmail.com>
2020-05-12 17:51:13 -0700

    remove config dir

--- airflow/plugins/__init__.py
+++ airflow/plugins/__init__.py
@@ -1,10 +1,10 @@
-from plugins.stage_redshift import StageToRedshiftOperator
-from plugins.load_fact import LoadFactOperator
-from plugins.load_dimension import LoadDimensionOperator
-from plugins.data_quality import DataQualityOperator
-from plugins.created_table_check import CreatedTableOperator
-from plugins.stl_load_errors_tables import CreateSTLErrorTablesOperator
-from plugins.fact_branch import FactBranchOperator
+from stage_redshift import StageToRedshiftOperator
+from load_fact import LoadFactOperator
+from load_dimension import LoadDimensionOperator
+from data_quality import DataQualityOperator
+from created_table_check import CreatedTableOperator
+from stl_load_errors_tables import CreateSTLErrorTablesOperator
+from fact_branch import FactBranchOperator
 __all__ = [
     'StageToRedshiftOperator',
     'LoadFactOperator',
